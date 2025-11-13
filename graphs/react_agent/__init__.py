8e97e6190693a26e8d7b7487ad949ae6f450f093
Vitalii Kondratenko <dev.vk1@solwey.com>
2025-11-13 13:20:28 +0200

    chore: rename tools agent to reAct agent

--- graphs/react_agent/__init__.py
+++ graphs/react_agent/__init__.py
@@ -1,9 +1,3 @@
-"""React Agent.
-
-This module defines a custom reasoning and action agent graph.
-It invokes tools in a simple loop.
-"""
-
-from react_agent.graph import graph
+from graphs.react_agent.graph import graph
 
 __all__ = ["graph"]
